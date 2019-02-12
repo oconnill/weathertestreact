b64977e8d2dd6bc0800400626991c0a79de69696
oconnill <doconnelldesign@gmail.com>
2019-02-11 20:53:35 -0700

    learning weather tutorial

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDom from "react-dom"
+import SeasonDisplay from "./SeasonDisplay"
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// const App = () => {
+//
+//
+//
+//
+//   return (
+//   <div>
+//     <div>Hello</div>
+//     <SeasonDisplay />
+//   </div>
+//     )
+// }
+
+
+class App extends React.Component {
+  constructor(props){
+      super(props);
+
+      this.state = { lat: null };
+
+      window.navigator.geolocation.getCurrentPosition(
+        (position) => {
+          this.setState({
+              lat: position.coords.latitude
+            })
+          },
+        (err) => console.log(err)
+        )
+}
+
+
+render () {
+  return <div> Latitude: { this.state.lat } </div>
+};
+}
+
+
+
+
+
+
+ReactDom.render(
+  <App />,
+  document.querySelector("#root")
+  );
