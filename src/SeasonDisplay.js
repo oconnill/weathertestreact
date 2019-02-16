9a18bdb724261529d9cb6fcc0d6a1f8f5ad19faa
oconnill <doconnelldesign@gmail.com>
2019-02-16 13:29:16 -0700

    Your Message

---
+++ src/SeasonDisplay.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const getSeason = (lat, month) => {
+if(month > 2 && month < 9){
+  return lat > 0 ? "summer" : "winter";
+}
+else{
+  return lat < 0 ? "winter" : "summer";
+}
+}
+
+const SeasonDisplay = (props) => {
+
+  const season = getSeason(props.lat, new Date().getMonth())
+
+  return (
+    <div>The weather for you is in: { season }</div>
+    )
+}
+
+export default SeasonDisplay
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
-import ReactDom from "react-dom"
-import SeasonDisplay from "./SeasonDisplay"
-
+import ReactDom from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
 // const App = () => {
 //
@@ -16,35 +15,28 @@ import SeasonDisplay from "./SeasonDisplay"
 //     )
 // }
 
-
 class App extends React.Component {
-  constructor(props){
-      super(props);
 
-      this.state = { lat: null };
+state = { lat: null, errorMessage: "" };
 
-      window.navigator.geolocation.getCurrentPosition(
-        (position) => {
-          this.setState({
-              lat: position.coords.latitude
-            })
-          },
-        (err) => console.log(err)
-        )
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  render() {
+      if(this.state.errorMessage && !this.state.lat){
+      return <div>Error: {this.state.errorMessage}</div>;
+      }
+      if(!this.state.errorMessage && this.state.lat){
+      return <SeasonDisplay lat={ this.state.lat }/>
+      }
+
+      return <div>Loading ...</div>
+
+  }
 }
 
-
-render () {
-  return <div> Latitude: { this.state.lat } </div>
-};
-}
-
-
-
-
-
-
-ReactDom.render(
-  <App />,
-  document.querySelector("#root")
-  );
+ReactDom.render(<App />, document.querySelector("#root"));
